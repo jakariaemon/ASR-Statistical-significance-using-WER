abf630dc0b0025701bf4640b69bd95bee9105634
emon-j <jakariaemon1521@gmail.com>
2024-01-28 18:03:52 +0900

    add sample test

---
+++ test2.py
@@ -0,0 +1,15 @@
+import numpy as np
+np.random.seed(42)
+
+from asr_stat_significance import StatisticalSignificance
+
+si_obj = StatisticalSignificance(
+    file_path="wer_cv.txt", 
+    total_batch=1000,
+    use_gaussian_appr=True,
+    sep='|',
+)
+ci_obj  = si_obj.compute_significance(
+                    num_samples_per_batch=30, confidence_level=0.99)
+print(ci_obj)
+print(f"The difference in WER between Model X and Y is significant: ", {ci_obj.is_significant()})
